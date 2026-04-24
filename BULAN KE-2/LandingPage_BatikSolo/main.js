083f7ce64d884c4b4b8616887ed2dcacfd4048db
AlVFino <backtoll82.com>
2026-04-24 23:38:55 +0700

    fx all project magang gamelab

---
+++ BULAN KE-2/LandingPage_BatikSolo/main.js
@@ -0,0 +1,88 @@
+    ScrollReveal().reveal('#navbar-example2', {
+      delay: 200,
+      distance: '50px',
+      origin: 'bottom',
+      duration: 500,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#section-hero', {
+      delay: 200,
+      distance: '50px',
+      origin: 'left',
+      duration: 500,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#tentangbatik', {
+      delay: 200,
+      distance: '50px',
+      origin: 'right',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#tentang', {
+      delay: 200,
+      distance: '50px',
+      origin: 'bottom',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });    
+    ScrollReveal().reveal('#produk', {
+      delay: 200,
+      distance: '50px',
+      origin: 'left',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#keunggulan', {
+      delay: 200,
+      distance: '50px',
+      origin: 'right',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#testimoni', {
+      delay: 200,
+      distance: '50px',
+      origin: 'left',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#berita', {
+      delay: 200,
+      distance: '50px',
+      origin: 'right',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#faq', {
+      delay: 200,
+      distance: '50px',
+      origin: 'left',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#kontak-maps', {
+      delay: 200,
+      distance: '50px',
+      origin: 'bottom',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
+    ScrollReveal().reveal('#footer', {
+      delay: 200,
+      distance: '50px',
+      origin: 'left',
+      duration: 1000,
+      easing: 'ease-in-out',
+      reset: true,
+    });
